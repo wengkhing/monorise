7923862cb8874cf347ccf42b487c3f416a0eb1ad
Weng Khing <wengkhing@gmail.com>
2025-06-06 07:13:45 +0800

    update projection expression

--- packages/core/data/ProjectionExpression.ts
+++ packages/core/data/ProjectionExpression.ts
@@ -2,7 +2,8 @@ export type ProjectionExpressionValues =
   (typeof PROJECTION_EXPRESSION)[keyof typeof PROJECTION_EXPRESSION];
 
 export const PROJECTION_EXPRESSION = {
-  NO_DATA: 'byEntityType, byEntityId, entityType, entityId, mutualId',
+  NO_DATA:
+    'byEntityType, byEntityId, entityType, entityId, mutualId, createdAt, updatedAt',
   MUTUAL_DATA_ONLY:
-    'byEntityType, byEntityId, entityType, entityId, mutualId, mutualData',
+    'byEntityType, byEntityId, entityType, entityId, mutualId, mutualData, createdAt, updatedAt',
 } as const;
